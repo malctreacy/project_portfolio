8ba5a9ac4702c19425cd3e96851a7a406934292d
Malcolm Treacy <mtreacy@Malcolms-MBP.T-mobile.com>
2019-05-16 14:10:57 -0700

    completed 'home' component

--- project-portfolio/src/app/core/app.component.ts
+++ project-portfolio/src/app/core/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'project-portfolio';
+  constructor() { }
+
+  words:any[]=["Web Developer", "Student", "Software Engineer"];
+
+  word:string="Software Engineer";
+  currentIdx:number =0;
+  x=5;
+
+  ngOnInit(){
+
+    setInterval(()=>{
+      this.rotateWords()
+    },this.x*1000)
+    }
+  rotateWords(){
+    if(this.currentIdx>=this.words.length){
+      this.currentIdx=0;
+    }
+    this.word=this.words[this.currentIdx];
+    this.currentIdx++
+  }
 }

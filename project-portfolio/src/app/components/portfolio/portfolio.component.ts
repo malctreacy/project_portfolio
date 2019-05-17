8f331fd017a0dcb64b158c02a5857a713c710f8d
Malcolm Treacy <mtreacy@dhcp-v121-004.mobile.uci.edu>
2019-05-17 15:32:34 -0700

    portfolio animations

--- project-portfolio/src/app/components/portfolio/portfolio.component.ts
+++ project-portfolio/src/app/components/portfolio/portfolio.component.ts
@@ -1,24 +1,60 @@
 import { Component, OnInit } from '@angular/core';
 import { PortfolioService } from 'src/app/services/portfolio.service';
 import { Portfolio } from 'src/app/services/portfolio.model';
+import { transition, trigger, style, animate, query, stagger, animateChild } from '@angular/animations';
 
 @Component({
   selector: 'app-portfolio',
   templateUrl: './portfolio.component.html',
-  styleUrls: ['./portfolio.component.scss']
+  styleUrls: ['./portfolio.component.scss'],
+  animations:[
+    trigger('list',[
+      transition(':enter', [
+        query('@items', stagger(60, animateChild()))
+      ])
+    ]),
+    trigger('items',[
+      transition(':enter',[
+        style({ transform:'scale(0)', opacity:0}),
+        animate('.0350s cubic-bezier(.8,-0.6, 0.2, 1.5)',
+          style({transform:'scale(1)', opacity: 1})),
+      ])
+    ])
+  ]
 })
+
 export class PortfolioComponent implements OnInit {
+
+  types: string[];
   // Only assignable
-  rooms = ['Room1', 'Room2', 'Room3'];
-  selectedType: 'all' | 'Angular' | 'React' | 'Vue' = 'all';
+  private _selectedType: string = 'all';
+  // getter method for selected types
+  get selectedType(){
+    return this._selectedType;
+  }
+  // setter method for new grouping
+  set selectedType(newValue:string){
+    if(newValue !== this._selectedType){
+      this._selectedType = newValue;
+      this.loadPortfolios(this._selectedType);
+    }
+  }
+
   portfolios: Portfolio[];
+
   // constructor only used for basic initializations
   constructor(private portfolioSvc:PortfolioService) { }
 
   // onInit is a provided as a lifecycle hooks
   ngOnInit() {
+    this.loadPortfolios(this._selectedType);
+  }
+
+  loadPortfolios(selectedType:string) {
     this.portfolioSvc.get().subscribe(data => {
-      this.portfolios = data;
+      // check that the first index of the value is a distinct value
+      this.types=data.map(p=>p.type).filter((value, index, self) => self.indexOf(value) === index);
+      this.portfolios = data.filter(p=>p.type === selectedType || selectedType === 'all');
     });
   }
 
